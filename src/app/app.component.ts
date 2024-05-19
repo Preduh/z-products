9fc60790657422a15b29b7cc8bbe84404ef638be
preduh <pedro.lucas5860@gmail.com>
2024-05-18 21:47:51 -0300

    feat: exhib all sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
+import { FiltersComponent } from './components/filters/filters.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainCategoriesComponent } from './components/main-categories/main-categories.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProductsComponent,
+    BannerComponent,
+    FiltersComponent,
+    MainCategoriesComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import {
+  Camera,
+  Joystick,
+  Laptop,
+  LucideAngularModule,
+  Smartphone,
+  Tv2,
+  Watch,
+} from 'lucide-angular';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        Watch,
+        Tv2,
+        Laptop,
+        Smartphone,
+        Camera,
+        Joystick,
+      })
+    ),
+  ],
 };
