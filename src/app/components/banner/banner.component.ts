412bf096e400c48a617bbb18d745e0d13ee08a03
preduh <pedro.lucas5860@gmail.com>
2024-05-18 23:49:53 -0300

    feat: add new banner layout

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, TagModule, ButtonModule],
   templateUrl: './banner.component.html',
+  host: { ngSkipHydration: 'true' },
 })
-export class BannerComponent {}
+export class BannerComponent implements OnInit {
+  products: Array<any>;
+
+  constructor() {
+    this.products = [];
+  }
+
+  ngOnInit(): void {
+    this.products = [
+      {
+        image: 'gaming-set.jpg',
+        inventoryStatus: 'primary',
+        name: 'Gaming Set',
+        price: 99.99,
+        color: 'bg-blue-500',
+      },
+      {
+        image: 'bamboo-watch.jpg',
+        inventoryStatus: 'success',
+        name: 'Bamboo Watch',
+        price: 59.99,
+        color: 'bg-blue-600',
+      },
+    ];
+  }
+}
