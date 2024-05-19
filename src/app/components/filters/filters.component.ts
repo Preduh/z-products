12e0b68fc15a4cfa739631705f3c6468980c2295
preduh <pedro.lucas5860@gmail.com>
2024-05-18 21:47:20 -0300

    feat: add temporary filter section

---
+++ src/app/components/filters/filters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './filters.component.html',
+})
+export class FiltersComponent {}
